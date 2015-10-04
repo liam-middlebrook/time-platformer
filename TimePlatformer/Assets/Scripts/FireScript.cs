447b3b290cd22b52c32c9f7cbe66c6e3779ab871
unknown <sbh9278@GDD-21.main.ad.rit.edu>
2015-10-04 14:58:45 -0400

    Added in main character asset, rescaled and refined the fire, vine, and wind assets to attain a pixilated look, and implemented the mechanics for vines, fire, and wind. Vines swap between two children of a larger vine object depending on day and night, fire simply appears during night, and wind will add forces in one of two directions when colliding with the player, depending on night or day.

---
+++ TimePlatformer/Assets/Scripts/FireScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		col.gameObject.transform.position = new Vector3 (0f, 1.5f, 0f);
+	}
+}
---
+++ TimePlatformer/Assets/Scripts/WindScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindScript : MonoBehaviour {
+
+	/// <summary>
+	/// the variable, which should be set externally, that denotes whether this is day wind or not.
+	/// </summary>
+	public bool day;
+
+	/// <summary>
+	/// The force.
+	/// </summary>
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D (Collider2D col){
+
+		if (day) {
+			Vector2 moveVec = new Vector2(12,0);
+			col.gameObject.GetComponent<Rigidbody2D>().AddForce(moveVec);
+		} else {
+			Vector2 moveVec = new Vector2(-12,0);
+			col.gameObject.GetComponent<Rigidbody2D>().AddForce(moveVec);
+		}
+	}
+}
