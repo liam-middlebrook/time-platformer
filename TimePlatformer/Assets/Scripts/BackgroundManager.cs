8d94b522df475c3886fbce32b3b151c6969ae9ea
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-10-05 17:46:12 -0400

    Added BackgroundManger
    This will allow for switching of background textures based on a day/night
    change.

---
+++ TimePlatformer/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public SpriteRenderer DayBackground;
+    public SpriteRenderer NightBackground;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Listen to the time changed event
+        TimeController.Instance.TimeChanged += TimeChanged;
+
+        // Set the intial state of the object based on what time the
+        // level is starting off as
+
+        TimeChanged(TimeController.Instance.CurrentTime);
+    }
+
+    // Handle switching for the time of day
+    void TimeChanged(TimeOfDay newTime)
+    {
+        DayBackground.enabled = (newTime == TimeOfDay.DAY);
+        NightBackground.enabled = (newTime == TimeOfDay.NIGHT);
+    }
+}
