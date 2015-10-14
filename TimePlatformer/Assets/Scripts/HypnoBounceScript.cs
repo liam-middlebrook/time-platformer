c320af5ec820fec6ba6b66063a076083df7ce2c5
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2015-10-14 18:15:26 -0400

    Added corrected work on Hypno and Knut without scene

---
+++ TimePlatformer/Assets/Scripts/HypnoBounceScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HypnoBounceScript : MonoBehaviour {
+
+    /// <summary>
+    /// The Amount the character bounces upon landing on the enemy
+    /// </summary>
+    public float bounceAmount;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        col.gameObject.GetComponent<Rigidbody2D>().AddForce(bounceAmount * Vector2.up);
+    }
+}
