6783da172b80f04bacda461f19208504673c9fe3
Chris Trantas <cmt4536@rit.edu>
2015-10-14 16:04:15 -0400

    Disabled all canvas layers. Added incomplete menu scripts: GameUIScript, MainMenuScript, and PauseMenuScript. They have psuedocode in them currently and are attached to the proper objects.

---
+++ TimePlatformer/Assets/Scripts/GameUIScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameUIScript : MonoBehaviour {
+	// get and set each star counter
+	int star = 0;
+	
+	///Summary
+	/// Pauses game and opens up the pause menu. Disables the Game UI button and hides it.
+	/// Summary end
+	
+	void PausemenuButton(){
+		
+	}
+
+}
---
+++ TimePlatformer/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+	// get and set each button inside of the start menu canvas
+	
+	///Summary
+	/// If start button is clicked it load the first level
+	/// then hide/disable or unload the start screen
+	/// Summary end
+	
+	void startButton(){
+
+	}
+	
+	///Summary - not sure if will be implemented
+	/// If level select button is clicked it will load a selected level
+	/// then hide/disable or unload the level selection screen
+	/// Summary end
+	
+	void levelSelectButton(){
+		
+	}
+
+	///Summary
+	// If exit game is clicked then the game will exit
+	/// Summary end
+	
+	void exitGame(){
+		exitGame ();
+	}
+}
