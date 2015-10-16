d877559f4cd8a885bec4007d5597cf9ec63c9ff4
unknown <jja6071@GDD-25.main.ad.rit.edu>
2015-10-16 17:12:54 -0400

    Second attempt on commiting the enemy assets

---
+++ TimePlatformer/Assets/Scripts/EnemyHorizontalMovementScript.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHorizontalMovementScript : MonoBehaviour {
+
+	/// <summary>
+	/// Used to change the movement based on time of day
+	/// </summary>
+	public TimeOfDay ActiveWhen;
+
+	/// <summary>
+	/// Determines if the enemy is active all the time
+	/// </summary>
+	public bool ActiveAllTheTime;
+
+	/// <summary>
+	/// The direction as an integer to change the direction of the enemy
+	/// </summary>
+    public int direction;
+
+	/// <summary>
+	/// The float value that determines the speed the enemy moves to reach the max range
+	/// </summary>
+    public float walkSpeed;
+
+	/// <summary>
+	/// The range the enemy will move in between
+	/// </summary>
+    public float maxRange;
+
+	/// <summary>
+	/// The position the enemy started at
+	/// </summary>
+    private Vector2 startPosition;
+
+	/// <summary>
+	/// The vector used for the enemy's translation
+	/// </summary>
+    private Vector2 walkingVector;
+
+	/// <summary>
+	/// Determines if the enemy is moving
+	/// </summary>
+	private bool isWalking;
+
+	// Use this for initialization
+	void Start () 
+	{
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isWalking || ActiveAllTheTime) 
+		{
+			Walk ();
+		}
+		
+		// Listen to the time changed event
+		TimeController.Instance.TimeChanged += TimeChanged;
+		
+		// Set the intial state of the object based on what time the
+		// level is starting off as
+		
+		TimeChanged(TimeController.Instance.CurrentTime);
+	}
+
+    void Walk()
+    {
+        if (Vector2.Distance(transform.position, startPosition) >= maxRange / 10.0f)
+		{
+			direction *= -1;
+			//transform.Translate(walkingVector * direction * Time.deltaTime);
+			Vector2 revScale = transform.localScale;
+			revScale.x *= -1.0f;
+			transform.localScale = revScale;
+        }
+
+        walkingVector = new Vector2(direction * walkSpeed, 0.0f);
+        transform.Translate(walkingVector * Time.deltaTime);
+    }
+
+	// Checks if the change of time will stop the enemy
+	void TimeChanged(TimeOfDay newTime)
+	{
+		isWalking = (newTime == ActiveWhen);
+	}
+
+	void OnDestroy()
+	{
+		TimeController.Instance.TimeChanged -= TimeChanged;
+	}
+}
---
+++ TimePlatformer/Assets/Scripts/HypnoBounceScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HypnoBounceScript : MonoBehaviour {
+
+	/// <summary>
+	/// The amount of bounce the character will go
+	/// </summary>
+    public float bounceAmount;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        col.gameObject.GetComponent<Rigidbody2D>().AddForce(bounceAmount * Vector2.up);
+    }
+}
