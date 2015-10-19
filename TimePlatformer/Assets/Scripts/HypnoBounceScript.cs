a038fcc66fd532818427d9545d59cf8c54ac9b8a
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2015-10-19 17:01:05 -0400

    Trying enemy asset commit again and again

--- TimePlatformer/Assets/Scripts/HypnoBounceScript.cs
+++ TimePlatformer/Assets/Scripts/HypnoBounceScript.cs
@@ -3,24 +3,9 @@ using System.Collections;
 
 public class HypnoBounceScript : MonoBehaviour {
 
-<<<<<<< HEAD
 	/// <summary>
 	/// The amount of bounce the character will go
 	/// </summary>
-    public float bounceAmount;
-
-	// Use this for initialization
-	void Start () 
-	{
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-=======
-    /// <summary>
-    /// The Amount the character bounces upon landing on the enemy
-    /// </summary>
     public float bounceAmount;
 
 	// Use this for initialization
@@ -29,7 +14,6 @@ public class HypnoBounceScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
->>>>>>> c320af5ec820fec6ba6b66063a076083df7ce2c5
 	
 	}
 
