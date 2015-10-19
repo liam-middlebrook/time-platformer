3a276afd5ca3f29d03be51e1ecb4245d6842436a
unknown <sbh9278@GDD-42.main.ad.rit.edu>
2015-10-19 15:16:08 -0400

    This adds in one of my scenes, but more importantly
    it adds prefabs for a spawner, standardizes the player
    prefab as the one we used in the last prototype, and
    also adds the spawner, the player, and a new underground
    ground tile to the tile editor.

---
+++ TimePlatformer/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (Player);
+		Player.transform.position = this.transform.position;
+	}
+}
