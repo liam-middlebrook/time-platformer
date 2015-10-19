5f0a7b9719ab10701f3945d685c1ce8b98a612fc
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2015-10-19 19:58:12 -0400

    Levels with enemies but has stack overflow and lag issues

--- TimePlatformer/Assets/Scripts/KillScript.cs
+++ TimePlatformer/Assets/Scripts/KillScript.cs
@@ -2,17 +2,18 @@
 using System.Collections;
 
 public class KillScript : MonoBehaviour {
-
+	
 	/// <summary>
 	/// The respawn location for the player after death.
 	/// </summary>
-	public Vector3 respawnLoc;
-
+	private GameObject respawn;
+	
 	/// <summary>
 	/// Checks for collision (with the player) and then resets the player back to a respanwn location.
 	/// </summary>
 	/// <param name="col">Col.</param>
 	void OnCollisionEnter2D (Collision2D col){
-		col.gameObject.transform.position = respawnLoc;
+		respawn = GameObject.FindGameObjectWithTag ("Spawner");
+		col.transform.position = respawn.transform.position;
 	}
 }
