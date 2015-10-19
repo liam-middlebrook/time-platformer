c83d4c35b9d2ae3934b43b4c9177c7d462c296ec
Jeremy Astolfi <jeremyjastolfi@gmail.com>
2015-10-19 16:37:35 -0400

    Added the assets to the updated master

---
+++ TimePlatformer/Assets/Scripts/KillScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour {
+
+	/// <summary>
+	/// The respawn location for the player after death.
+	/// </summary>
+	public Vector3 respawnLoc;
+
+	/// <summary>
+	/// Checks for collision (with the player) and then resets the player back to a respanwn location.
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnCollisionEnter2D (Collision2D col){
+		col.gameObject.transform.position = respawnLoc;
+	}
+}
