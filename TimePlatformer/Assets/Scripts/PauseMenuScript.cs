2381315e8005a4bfea28bd276756c3ce3e87228e
ChrisTrantas <cmt4536@rit.edu>
2015-10-21 23:46:51 -0400

    Implemented the menu and added prefab. There is a test build of it as well. Just drag and drop the prefab into a scene and you are good. Also it is set for 1600x900

--- TimePlatformer/Assets/Scripts/PauseMenuScript.cs
+++ TimePlatformer/Assets/Scripts/PauseMenuScript.cs
@@ -2,59 +2,67 @@
 using System.Collections;
 
 public class PauseMenuScript : MonoBehaviour {
-	// variables used
-	
-    // Render variable for each button
-    // When pause is rendered the rest are disabled
-    // when pause is not rendered render the rest of the menu
 
-	// Get player object to reset location later	
 
-    // If called render the other four buttons and disable
-    // pause button rendering as well as stop animations
-    void pause(){
+    public void pause(){
+        Debug.Log("pausing");
+        Time.timeScale = 0;
+        var continueButton = GameObject.FindGameObjectWithTag("ContinueButton");
+        var restartButton = GameObject.FindGameObjectWithTag("RestartButton");
+        var mainMenuButton = GameObject.FindGameObjectWithTag("MainMenuButton");
+        var exitButton = GameObject.FindGameObjectWithTag("ExitGameButton");
+        var pauseButton = GameObject.FindGameObjectWithTag("PauseButton");
 
-       // this.continueGame.enabled();
-       // this.restart.enabled();
-       // this.mainMenu.enabled();
-       // this.exit.enabled();
+
+        continueButton.transform.position = new Vector3(850.0f, 500.0f);
+        restartButton.transform.position = new Vector3(850.0f, 440.0f);
+        mainMenuButton.transform.position = new Vector3(850.0f, 380.0f);
+        exitButton.transform.position = new Vector3(850.0f, 320.0f);
+        pauseButton.transform.position = new Vector3(-10000.0f, 120.0f);
+    }                  
+
+
+    // If continue button is clicked it will unpause the level.
+    // It will enable pause button render and disable the other buttons as well as itself
+    // resume animation
+    public void continueGame(){
+        Time.timeScale = 1;
+        var continueButton = GameObject.FindGameObjectWithTag("ContinueButton");
+        var restartButton = GameObject.FindGameObjectWithTag("RestartButton");
+        var mainMenuButton = GameObject.FindGameObjectWithTag("MainMenuButton");
+        var exitButton = GameObject.FindGameObjectWithTag("ExitGameButton");
+        var pauseButton = GameObject.FindGameObjectWithTag("PauseButton");
+
+        continueButton.transform.position = new Vector3(-470.0f, 400.0f);
+        restartButton.transform.position = new Vector3(-470.0f, 340.0f);
+        mainMenuButton.transform.position = new Vector3(-470.0f, 280.0f);
+        exitButton.transform.position = new Vector3(-470.0f, 220.0f);
+        pauseButton.transform.position = new Vector3(1500.0f, 20.0f);
+
+
     }
-
-
-	// If continue button is clicked it will unpause the level.
-	// It will enable pause button render and disable the other buttons as well as itself
-	// resume animation
-	void continueGame(){
-        //this.pause.enabled();
-        //this.continueGame.disabled();
-        //this.restart.disabled();
-        //this.mainMenu.disabled();
-        //this.exit.disabled();
-	}
 
 
 	// If restart button is clicked it will restart the level.
     // It will enable pause button render and disable the other buttons as well as itself
-	void restart(){
-        //this.pause.enabled();
-        //this.continueGame.disabled();
-        //this.restart.disabled();
-        //this.mainMenu.disabled();
-        //this.exit.disabled();
-	}
+	public void restart(){
+        var lastLvl = Application.loadedLevel;
+        Application.LoadLevel(lastLvl);
+    }
 
 
 	// If main menu button is clicked it will load the main menu
-	void mainMenu(){
+	public void mainMenu(){
+        Application.LoadLevel(0);
 
-	}
+    }
 
 
 	///Summary
 	// If exit game is clicked then the game will exit
 	/// Summary end
 
-	void exit(){
+	public void exit(){
         Application.Quit();
         Debug.Log("exit called");
 	}
