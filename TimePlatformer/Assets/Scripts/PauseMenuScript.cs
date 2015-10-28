9533abda5426f33ad152a3911bcb6575bee8bddb
ChrisTrantas <cmt4536@rit.edu>
2015-10-28 17:51:47 -0400

    Made menu prefab and cleaned up PauseMenuScript.

--- TimePlatformer/Assets/Scripts/PauseMenuScript.cs
+++ TimePlatformer/Assets/Scripts/PauseMenuScript.cs
@@ -1,51 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PauseMenuScript : MonoBehaviour {
+public class PauseMenuScript : MonoBehaviour 
+{
+    // Public GameObjects to set buttons
+    public GameObject continueButton;
+    public GameObject restartButton;
+    public GameObject mainMenuButton;
+    public GameObject exitButton;
+    public GameObject pauseButton;
 
-
-    public void pause(){
-        Debug.Log("pausing");
-        Time.timeScale = 0;
-        var continueButton = GameObject.FindGameObjectWithTag("ContinueButton");
-        var restartButton = GameObject.FindGameObjectWithTag("RestartButton");
-        var mainMenuButton = GameObject.FindGameObjectWithTag("MainMenuButton");
-        var exitButton = GameObject.FindGameObjectWithTag("ExitGameButton");
-        var pauseButton = GameObject.FindGameObjectWithTag("PauseButton");
-
-
-        continueButton.transform.position = new Vector3(850.0f, 500.0f);
-        restartButton.transform.position = new Vector3(850.0f, 440.0f);
-        mainMenuButton.transform.position = new Vector3(850.0f, 380.0f);
-        exitButton.transform.position = new Vector3(850.0f, 320.0f);
-        pauseButton.transform.position = new Vector3(-10000.0f, 120.0f);
+    // Pauses the game and shows the menu
+    public void Pause()
+    {
+       Time.timeScale = 0;
+        
+       continueButton.SetActive(true);
+       restartButton.SetActive(true);
+       mainMenuButton.SetActive(true);
+       exitButton.SetActive(true);
+       pauseButton.SetActive(false);
     }                  
 
 
     // If continue button is clicked it will unpause the level.
     // It will enable pause button render and disable the other buttons as well as itself
     // resume animation
-    public void continueGame(){
+    public void ContinueGame()
+    {
         Time.timeScale = 1;
-        var continueButton = GameObject.FindGameObjectWithTag("ContinueButton");
-        var restartButton = GameObject.FindGameObjectWithTag("RestartButton");
-        var mainMenuButton = GameObject.FindGameObjectWithTag("MainMenuButton");
-        var exitButton = GameObject.FindGameObjectWithTag("ExitGameButton");
-        var pauseButton = GameObject.FindGameObjectWithTag("PauseButton");
 
-        continueButton.transform.position = new Vector3(-470.0f, 400.0f);
-        restartButton.transform.position = new Vector3(-470.0f, 340.0f);
-        mainMenuButton.transform.position = new Vector3(-470.0f, 280.0f);
-        exitButton.transform.position = new Vector3(-470.0f, 220.0f);
-        pauseButton.transform.position = new Vector3(1500.0f, 20.0f);
-
-
+        continueButton.SetActive(false);
+        restartButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+        exitButton.SetActive(false);
+        pauseButton.SetActive(true);
     }
 
 
 	// If restart button is clicked it will restart the level.
     // It will enable pause button render and disable the other buttons as well as itself
-	public void restart(){
+	public void Restart()
+    {
         var lastLvl = Application.loadedLevel;
         Application.LoadLevel(lastLvl);
         Time.timeScale = 1;
@@ -53,10 +49,10 @@ public class PauseMenuScript : MonoBehaviour {
 
 
 	// If main menu button is clicked it will load the main menu
-	public void mainMenu(){
+	public void MainMenu()
+    {
         Application.LoadLevel(0);
         Time.timeScale = 1;
-
     }
 
 
@@ -64,8 +60,8 @@ public class PauseMenuScript : MonoBehaviour {
 	// If exit game is clicked then the game will exit
 	/// Summary end
 
-	public void exit(){
+	public void Exit()
+    {
         Application.Quit();
-        Debug.Log("exit called");
 	}
 }
